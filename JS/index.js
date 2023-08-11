e333b880412767e777a9ce8ccb585cd192eaf22c
jvlazar <jvlazar@gmail.com>
2023-08-11 15:41:31 -0600

    keep active tab in session storage

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,44 @@
 function openTab(evt, tabName) {
+  sessionStorage.clear();
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablinks");
+
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    sessionStorage.removeItem(tablinks[i])
   }
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 
 
-  // console.log(`the current active tab is ${tabName}`);
 
   // save active tab to local storage
+ 
+  sessionStorage.setItem("active", tabName);
+}
+
+
+
+function main(){
+
+  
+  window.addEventListener("load", (event) => {
+    if (sessionStorage.getItem("active") == null){
+      document.getElementById('about_button').click();
+  } else {
+    let activeTab = sessionStorage.getItem("active").substring(0, sessionStorage.getItem("active").indexOf('_')) + "_button";
+    
+    document.getElementById(activeTab).click();
+  }
+
+  });
 
 }
 
 
+main();
 
