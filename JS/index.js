51b4319fba069850d92fcd5d8cf430b47b056abf
jvlazar <56981054+jvlazar@users.noreply.github.com>
2025-01-08 20:27:38 +0000

    start work on modal

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,6 @@
+let activeModal = null;
+let activeProject = null;
+
 function openTab(evt, tabName) {
   sessionStorage.clear();
   var i, tabcontent, tablinks;
@@ -21,11 +24,43 @@ function openTab(evt, tabName) {
   sessionStorage.setItem("active", tabName);
 }
 
+function showModal(clicked_div){
+  console.log(`clicked`);
+  const project_name = clicked_div.id.replace("project_", "")
+  activeProject = project_name;
+  const modal = document.getElementById(`modal_${project_name}`);
+  console.log(modal);
+  modal.style.display = "flex";
+  activeModal = modal;
+  document.activeElement.blur()
+}
 
+// closing modal on clicking the span
+const closeModal = function() {
+  console.log(`closing ${activeModal.id}`);
+  document.getElementById(activeModal.id).style.display = "none";
+  activeModal = null;
+}
+
+// closing modal on clicking outside of it
 
 function main(){
 
-  
+  window.addEventListener("click", function(e){
+    let modal = activeModal;
+    console.log(document.getElementById(modal.id).style.display);
+
+    if (modal != null){
+        if(document.getElementById(modal.id).contains(e.target)) {
+        } 
+        // if modal is open
+        else if (document.getElementById(modal.id).style.display == "flex"){
+          // closeModal();  
+      }
+    } else {
+    }
+  })
+ 
   window.addEventListener("load", (event) => {
     if (sessionStorage.getItem("active") == null){
       document.getElementById('about_button').click();
@@ -38,7 +73,6 @@ function main(){
   });
 
 }
-
 
 main();
 
