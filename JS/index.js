8164ffdb1691dc5445ae16605805cf6d8b942d1e
jvlazar <56981054+jvlazar@users.noreply.github.com>
2025-01-14 14:09:32 +0000

    close modal when clicking outside of modal

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,8 @@
-// import data from "./project_specifics.json"
 let activeModal = null;
 let activeProject = null;
 let activeTab = null;
 let projectData = null;
-// function openTab(evt, tabName) {
-//   sessionStorage.clear();
-//   var i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-//   tablinks = document.getElementsByClassName("tablinks");
 
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     sessionStorage.removeItem(tablinks[i])
-//   }
-//   document.getElementById(tabName).style.display = "block";
-//   evt.currentTarget.className += " active";
 
 onload = (event) => {
   if (localStorage.getItem("active tab") != "null"){
@@ -27,10 +12,6 @@ onload = (event) => {
   }
 }
 
-//   // save active tab to local storage
- 
-//   sessionStorage.setItem("active", tabName);
-// }
 
 // set active tab
 function setActive(page){
@@ -46,85 +27,47 @@ function setActive(page){
 
 
 function showModal(clicked_div){
-  const project_name = clicked_div.id.replace("project_", "")
-  activeProject = project_name;
-  const modal = document.getElementById(`modal_${project_name}`);
+  activeProject = clicked_div.id.replace("project_", "");
+  const modal = document.getElementById(`modal_${activeProject}`);
   modal.style.display = "grid";
   activeModal = modal;
+  document.getElementById(`${activeModal.id}_container`).style.display = "grid";
   document.activeElement.blur();
-
   showData();
 }
 
 // closing modal on clicking the span
 function closeModal() {
-  document.getElementById(activeModal.id).style.display = "none";
-  document.getElementById("content").style.filter = "none";
-  const ele = document.getElementsByClassName('project');
-  for (let index = 0; index < ele.length; index++) {
-    document.getElementById(ele[index].id).style.filter = "none";
+  if (activeModal != null && document.getElementById(activeModal.id).style.display == "grid"){
+    document.getElementById(activeModal.id).style.display = "none";
+    document.getElementById(`${activeModal.id}_container`).style.display = "none";
   }
-  activeModal = null;
 }
 
+
+// close modal by clicking outside of it
+window.onclick = function(event) {
+  myModal = document.getElementById(`${activeModal.id}_container`);
+  if (event.target == myModal) {
+    myModal.style.display = "none";
+  }
+} 
 
 async function showData(){
-console.log(`active modal is ${activeModal.id}`);
-    let data = await fetch('./JS/project_specifics.json')
-      .then(response => response.json());
-      for (let index = 0; index < data.length; index++){
-        console.log(data[index].modal);
-        if (data[index].modal == activeModal.id){
-          console.log(`they are the same`);
-          document.getElementsByClassName("languages")[index].innerHTML = data[index].language;
-          document.getElementsByClassName("modal_info")[index].innerHTML = data[index].description;
-          document.getElementsByClassName("link")[index].href = data[index].link;
-          if (data[index].modal.tools != "null"){
-            document.getElementsByClassName("tools")[index].innerHTML = data[index].tools;
+      let data = await fetch('./JS/project_specifics.json')
+        .then(response => response.json());
+        for (let index = 0; index < data.length; index++){
+          if (data[index].modal == activeModal.id){
+            document.getElementsByClassName("languages")[index].innerHTML = "Languages: " + data[index].language;
+            document.getElementsByClassName("modal_info")[index].innerHTML ="Project Description: <br> " + data[index].description;
+            document.getElementsByClassName("link")[index].href = data[index].link;
+            if (data[index].tools != null){
+              document.getElementsByClassName("tools")[index].innerHTML = "Additional Tools: " + data[index].tools;
+            } else{
+              document.getElementsByClassName("tools")[index].innerHTML = "Additional Tools: None";
+            }
           }
-        }
-
-      }
-
-      
-      // console.log(document.getElementsByClassName("modal_info")[0]);
-      
-     
+        }     
 }
-
-
-function main(){
-  window.addEventListener("click", function(e){
-    if (activeTab == "name_card_projects"){
-      let modal = activeModal;
-      if (modal != null){
-          if(document.getElementById(modal.id).contains(e.target)) {
-          } 
-          // if modal is open
-          else if (document.getElementById(modal.id).style.display == "flex"){
-            // closeModal();  
-        }
-      } else {
-      }
-      
-    }
-  })
-
-  
   
  
-  // window.addEventListener("load", (event) => {
-  //   if (sessionStorage.getItem("active") == null){
-  //     document.getElementById('about_button').click();
-  // } else {
-  //   let activeTab = sessionStorage.getItem("active").substring(0, sessionStorage.getItem("active").indexOf('_')) + "_button";
-    
-  //   document.getElementById(activeTab).click();
-  // }
-
-  // });
-
-}
-
-main();
-
